2c2eb0f2040baac00848ca6ee45b55d00b7df283
MaheshTiwaree <mahesh.tiwari@bugendaitech.com>
2022-11-08 07:41:18 +0530

    added gold color in theme

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -14,7 +14,7 @@ const Stats = () => {
           <h4 className="font-poppins font-semibold text-[30px] xs:text-[40px] leading-[43px] xs:leading-[26px] text-white">
             {stat.value}
           </h4>
-          <p className="font-poppins font-normal text-[15px] xs:text-[20px] leading-[21px] xs:leading-[26px] text-gradient uppercase ml-3">
+          <p className="font-poppins font-normal text-[15px] xs:text-[20px] leading-[21px] xs:leading-[26px] text-[gold] uppercase ml-3">
             {stat.title}
           </p>
         </div>
